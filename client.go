d27d1eb36bec7dc085ac8be35deafae0b5e16743
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-03-10 22:08:17 +0100

    update client.go

--- client.go
+++ client.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license,
+// license that can be found in the LICENSE file.
+
 package ssh
 
 import (
