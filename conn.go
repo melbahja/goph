1be9791196da7bd36c4b01267052ac287c280784
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-04-04 22:03:39 +0100

    add: connect funcs.

---
+++ conn.go
@@ -0,0 +1,29 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license.
+
+package goph
+
+import (
+	"fmt"
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	UDP string = "udp"
+	TCP string = "tcp"
+)
+
+// Get new connection.
+func Conn(c *Client, cfg *ssh.ClientConfig) (err error) {
+
+	if c.Port == 0 {
+		c.Port = 22
+	}
+
+	if c.Proto == "" {
+		c.Proto = TCP
+	}
+
+	c.Conn, err = ssh.Dial(c.Proto, fmt.Sprintf("%s:%d", c.Addr, c.Port), cfg)
+	return
+}
