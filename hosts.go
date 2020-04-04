c387e31b331c9c84517fd968ba784bac0e9881da
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-04-04 22:04:34 +0100

    add: known hosts callback.

---
+++ hosts.go
@@ -0,0 +1,23 @@
+// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Use of this source code is governed by a MIT license.
+
+package goph
+
+import (
+	"os"
+	"strings"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+)
+
+// Use default known hosts files to verify connection host key.
+func DefaultKnownHosts() (ssh.HostKeyCallback, error) {
+
+	return KnownHosts(strings.Join([]string{os.Getenv("HOME"), ".ssh", "known_hosts"}, "/"))
+}
+
+// Get known hosts callback from custom path.
+func KnownHosts(kh string) (ssh.HostKeyCallback, error) {
+
+	return knownhosts.New(kh)
+}
