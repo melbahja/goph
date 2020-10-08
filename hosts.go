964e6df644c19f3d9d6537861762d3e96ec6636e
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-10-08 16:05:04 +0100

    add name to returns

--- hosts.go
+++ hosts.go
@@ -16,13 +16,11 @@ var defaultPath = os.ExpandEnv("$HOME/.ssh/known_hosts")
 
 // Use default known hosts files to verify host public key.
 func DefaultKnownHosts() (ssh.HostKeyCallback, error) {
-
 	return KnownHosts(defaultPath)
 }
 
 // Get known hosts callback from a custom path.
 func KnownHosts(file string) (ssh.HostKeyCallback, error) {
-
 	return knownhosts.New(file)
 }
 
@@ -30,12 +28,9 @@ func KnownHosts(file string) (ssh.HostKeyCallback, error) {
 // It returns is the host found in known_hosts file and error,
 // If the host found in known_hosts file and error not nil that means public key mismatch, Maybe
 // MAN IN THE MIDDLE ATTACK! you should not handshake.
-func CheckKnownHost(host string, remote net.Addr, key ssh.PublicKey, knownFile string) (bool, error) {
+func CheckKnownHost(host string, remote net.Addr, key ssh.PublicKey, knownFile string) (found bool, err error) {
 
-	var (
-		hostErr error
-		keyErr  *knownhosts.KeyError
-	)
+	var keyErr *knownhosts.KeyError
 
 	// Fallback to default known_hosts file
 	if knownFile == "" {
@@ -43,29 +38,29 @@ func CheckKnownHost(host string, remote net.Addr, key ssh.PublicKey, knownFile s
 	}
 
 	// Get host key callback
-	callback, hostErr := KnownHosts(knownFile)
+	callback, err := KnownHosts(knownFile)
 
-	if hostErr != nil {
-		return false, hostErr
+	if err != nil {
+		return false, err
 	}
 
 	// check if host already exists.
-	hostErr = callback(host, remote, key)
+	err = callback(host, remote, key)
 
 	// Known host already exists.
-	if hostErr == nil {
+	if err == nil {
 		return true, nil
 	}
 
 	// Make sure that the error returned from the callback is host not in file error.
 	// If keyErr.Want is greater than 0 length, that means host is in file with different key.
-	if errors.As(hostErr, &keyErr) && len(keyErr.Want) > 0 {
+	if errors.As(err, &keyErr) && len(keyErr.Want) > 0 {
 		return true, keyErr
 	}
 
 	// Some other error occured and safest way to handle is to pass it back to user.
-	if hostErr != nil {
-		return false, hostErr
+	if err != nil {
+		return false, err
 	}
 
 	// Key is not trusted because it is not in the file.
@@ -73,27 +68,24 @@ func CheckKnownHost(host string, remote net.Addr, key ssh.PublicKey, knownFile s
 }
 
 // Add a host to knows hosts
-// this function by @dixonwille see: https://github.com/melbahja/goph/issues/2
-func AddKnownHost(host string, remote net.Addr, key ssh.PublicKey, knownFile string) error {
-
-	var fileErr error
+func AddKnownHost(host string, remote net.Addr, key ssh.PublicKey, knownFile string) (err error) {
 
 	// Fallback to default known_hosts file
 	if knownFile == "" {
 		knownFile = defaultPath
 	}
 
-	f, fileErr := os.OpenFile(knownFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	f, err := os.OpenFile(knownFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
 
-	if fileErr != nil {
-		return fileErr
+	if err != nil {
+		return err
 	}
 
 	defer f.Close()
 
 	knownHost := knownhosts.Normalize(remote.String())
 
-	_, fileErr = f.WriteString(knownhosts.Line([]string{knownHost}, key) + "\n")
+	_, err = f.WriteString(knownhosts.Line([]string{knownHost}, key) + "\n")
 
-	return fileErr
+	return err
 }
