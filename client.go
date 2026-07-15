671892dd38799c26e16018ff73685aeef7ebfb09
Mohamed Elbahja <bm9qdW5r@gmail.com>
2026-07-15 18:45:49 +0100

    update head year

--- client.go
+++ client.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Mohammed El Bahja. All rights reserved.
+// Copyright 2026 Mohammed El Bahja. All rights reserved.
 // Use of this source code is governed by a MIT license.
 
 package goph
